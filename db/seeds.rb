78f8927d7fe33bc792e023ec341ddd655b41aaef
MOHAMMAD ELMAHI <g00407950@atu.ie>
2026-03-06 23:49:15 +0000

    Remove NC-17 from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,8 +13,6 @@
 # Clear existing movies
 Movie.destroy_all
 
-ratings = ["G", "PG", "PG-13", "R", "NC-17"]
-
 movies_data = [
   { title: "The Lion King", rating: "G", description: "Animated classic", release_date: "1994-06-24" },
   { title: "Toy Story", rating: "G", description: "Animated adventure", release_date: "1995-11-22" },
@@ -22,10 +20,8 @@ movies_data = [
   { title: "Jurassic Park", rating: "PG-13", description: "Dinosaurs and action", release_date: "1993-06-11" },
   { title: "The Avengers", rating: "PG-13", description: "Superheroes assemble", release_date: "2012-05-04" },
   { title: "The Godfather", rating: "R", description: "Mafia classic", release_date: "1972-03-24" },
-  { title: "Deadpool", rating: "R", description: "Action comedy", release_date: "2016-02-12" },
-  { title: "Showgirls", rating: "NC-17", description: "Adult drama", release_date: "1995-09-22" }
+  { title: "Deadpool", rating: "R", description: "Action comedy", release_date: "2016-02-12" }
 ]
 
 movies_data.each { |m| Movie.create!(m) }
-
 puts "Seeded #{Movie.count} movies!"
